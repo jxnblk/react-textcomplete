b7f11a5b3f472f74a4ea345390cf312c57ba8428
Brent Jackson <jxnblk@github.com>
2015-02-19 18:23:07 -0500

    keyboards and stuff

--- textcomplete.js
+++ textcomplete.js
@@ -7,6 +7,7 @@
 // - handle return characters
 // - handle key events
 // - positioning based on cursor
+// - prop for max number of matches to show in menu list
 
 var React = require('react');
 var fuzzy = require('fuzzy');
@@ -16,10 +17,11 @@ module.exports = React.createClass({displayName: "exports",
   getInitialState: function() {
     return {
       value: this.props.value,
-      words: {},
+      words: [],
       currentWord: 0,
       cursorPosition: 0,
-      matches: []
+      matches: [],
+      selectedMatch: -1,
     }
   },
 
@@ -37,6 +39,7 @@ module.exports = React.createClass({displayName: "exports",
 
   parseWords: function(string) {
     var words = string.split(' ');
+    var words = string.split(/[\n\r\s]+/);
     var position = 0;
     words.map(function(word, i) {
       var wordobj = {
@@ -60,6 +63,7 @@ module.exports = React.createClass({displayName: "exports",
       }
     });
     this.setState({ currentWord: currentWord });
+    this.getMenuPosition();
     return currentWord;
   },
 
@@ -75,14 +79,31 @@ module.exports = React.createClass({displayName: "exports",
   completeWord: function(word) {
     var words = this.state.words;
     var value = '';
+    var $textarea = this.refs.textarea.getDOMNode();
+    //var windowYPosition = window.pageYOffset;
     words[this.state.currentWord].value = word;
     words.map(function(word) {
       value += word.value + ' ';
     });
     this.setState({
       value: value,
-      matches: []
+      matches: [],
+      selectedMatch: -1,
+    }, function() {
+      $textarea.focus();
+      //window.scrollTo(0, windowYPosition);
     });
+  },
+
+  getMenuPosition: function() {
+    var $dummy = this.refs.dummy.getDOMNode();
+    var dummyHTML = '';
+    this.state.words.forEach(function(word) {
+      dummyHTML += '<span>' + word.value + '</span> ';
+    });
+    $dummy.innerHTML = dummyHTML;
+    var $dummySpans = $dummy.querySelectorAll('span');
+    console.log($dummySpans, $dummySpans.item(this.state.currentWord));
   },
 
   handleChange: function(e) {
@@ -97,51 +118,117 @@ module.exports = React.createClass({displayName: "exports",
     });
   },
 
+  selectNextMatch: function() {
+    var index = this.state.selectedMatch;
+    if (index < this.state.matches.length - 1) {
+      index++;
+    }
+    this.setState({ selectedMatch: index });
+    return index;
+  },
+
+  selectPreviousMatch: function() {
+    var index = this.state.selectedMatch;
+    if (index > 0) {
+      index--;
+    }
+    this.setState({ selectedMatch: index });
+    return index;
+  },
+
   handleKeyDown: function(e) {
     var textarea = this.refs.textarea.getDOMNode();
-    var multiselect = this.refs.multiselect.getDOMNode();
-    var firstOption = multiselect.querySelector('option');
+    var menu = this.refs.menu.getDOMNode();
+    var links = menu.querySelectorAll('a');
+    var index = this.state.selectedMatch;
+    var windowYPosition = window.pageYOffset;
     switch (e.which) {
       case 40:
-        multiselect.focus();
-        multiselect.selectedIndex = 0;
+        e.preventDefault();
+        var index = this.selectNextMatch();
+        var link = links.item(index);
+        link.focus();
+        window.scrollTo(0, windowYPosition);
+        break;
+      case 38:
+        e.preventDefault();
+        var index = this.selectPreviousMatch();
+        var link = links.item(index);
+        link.focus();
+        window.scrollTo(0, windowYPosition);
         break;
     }
   },
 
+  componentDidMount: function() {
+    //if (!document) return false;
+    //document.onKeydown = this.handleKeyDown;
+    this.getMenuPosition();
+  },
+
   render: function() {
     var self = this;
-    var renderOptions = function(val) {
+    var renderItems = function(val, i) {
+      var key = 'match-' + i;
+      var handleClick = function(e) {
+        self.completeWord(e.target.innerText);
+      };
+      var linkClass = 'list-group-item ';
+      if (self.state.selectedMatch == i) {
+        linkClass += 'active';
+      }
       return (
-        React.createElement("option", {value: val}, val)
+        React.createElement("a", {href: "#!", 
+          key: key, 
+          ref: val, 
+          onKeyDown: this.handleKeyDown, 
+          className: linkClass, 
+          onClick: handleClick}, 
+          val
+        )
       );
     };
-    var selectStyle = {
+
+    var containerStyle = {
+      //position: 'relative',
+    };
+    var textareaStyle = {
+    };
+    var listStyle = {
       display: this.state.matches.length ? '' : 'none'
     };
-    var handleSelection = function(e) {
-      e.preventDefault();
-      console.log('selected', e.target.value);
-      self.completeWord(e.target.value);
+
+    var dummyValue = this.state.value;
+    // Copy textarea style and className
+    var dummyStyle = {
+      color: 'white',
+      backgroundColor: 'tomato',
+      opacity: .5,
+      position: 'absolute',
+      bottom: 0
     };
 
     return (
-      React.createElement("div", {className: "mb4"}, 
-        /* Pass parent props */
-        React.createElement("textarea", {
-          className: "form-control", 
+      React.createElement("div", {className: "mb4", style: containerStyle}, 
+        React.createElement("textarea", React.__spread({}, 
+          this.props, 
+          {className: "form-control", 
+          style: textareaStyle, 
           ref: "textarea", 
           value: this.state.value, 
           onChange: this.handleChange, 
-          onKeyDown: this.handleKeyDown}
-        ), 
-        /* Use ul with links */
-        React.createElement("select", {multiple: true, 
-          ref: "multiselect", 
-          style: selectStyle, 
-          onChange: handleSelection, 
-          className: "form-control"}, 
-          this.state.matches.map(renderOptions)
+          onKeyDown: this.handleKeyDown})
+          ), 
+        React.createElement("textarea", {
+          ref: "dummy", 
+          className: "form-control", 
+          style: dummyStyle, 
+          readOnly: true}), 
+        React.createElement("div", {ref: "menu", 
+          className: "list-group", 
+          onKeyDown: this.handleKeyDown, 
+          style: listStyle}, 
+          this.state.matches.map(renderItems)
         )
       )
     )
